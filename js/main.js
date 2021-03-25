20ae942d8c7128c3d3b6427c75f4625cc6176749
Tori <ttang1135@naver.com>
2021-03-26 00:23:43 +0900

    add 상단 이동 버튼

--- js/main.js
+++ js/main.js
@@ -16,22 +16,36 @@ searchInputEl.addEventListener('blur', function(){
 })
 
 const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector("#to-top");
 window.addEventListener('scroll', _.throttle(function(){
   if(window.scrollY>500){
     // badge숨기기
     gsap.to(badgeEl, .6,{
       opacity:0,
       display:'none'
-    })  
+    })
+    //버튼 보이기
+    gsap.to("#to-top", .2,{
+      x : 0
+    }) 
   }
   else{
     //badge보여주기
     gsap.to(badgeEl, .6,{
       opacity:1,
       display:'block'
+    })
+    //버튼 숨기기
+    gsap.to(toTopEl, .2,{
+      x : 100
     }) 
   }
 },300))
+toTopEl.addEventListener('click', function(){
+  gsap.to(window, .7, {
+    scrollTo :0 
+  })
+})
 
 // visual section fade in animation
 const fadeEls = document.querySelectorAll('.visual .fade-in');
