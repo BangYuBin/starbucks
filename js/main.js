9a8b4dbdca833baf3b2c93234832a8b7483cd357
Tori <ttang1135@naver.com>
2021-03-25 23:51:13 +0900

    add awards section

--- js/main.js
+++ js/main.js
@@ -67,6 +67,17 @@ new Swiper('.promotion .swiper-container',{
     nextEl: '.swiper-next'
   }
 });
+//awards slide
+new Swiper('.awards .swiper-container',{
+  autoplay : true,
+  loop : true,
+  spaceBetween : 30,
+  slidesPerView : 5,
+  navigation:{
+    prevEl :'.awards .swiper-prev' ,
+    nextEl: '.awards .swiper-next'
+  }
+})
 
 // promotion toggle
 const promotionEl = document.querySelector('.promotion');
